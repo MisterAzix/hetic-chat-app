b018a14581267b2214b7b93bb2b5d866fd1bdfd3
toutouff <63313022+toutouff@users.noreply.github.com>
2023-12-01 15:46:11 +0100

    feat(api): add of user service,module,controller

---
+++ apps/api/src/app/user/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+  async getUser(user_id: string) {
+    return this.prisma.user.findFirst({ where: { id: user_id } });
+  }
+}
