7385da40c28b86dd2890897451bcb3d0a3097e3f
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-12-02 17:54:28 +0100

    feat(api): adding swagger 📝

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,39 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 import { AppModule } from './app/app.module';
 
+const GLOBAL_PREFIX = 'api';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+  app.use(helmet());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('HETIC Chat App API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(GLOBAL_PREFIX, app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
   );
 }
 
