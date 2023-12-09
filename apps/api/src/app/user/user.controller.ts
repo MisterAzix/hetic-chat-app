93d4760ef55b23947e25994a7d17cbc002784170
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-12-09 17:56:03 +0100

    feat(api): adding user modification and deletion ✨ (#12)

--- apps/api/src/app/user/user.controller.ts
+++ apps/api/src/app/user/user.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dtos';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
   @Get(':id')
   getUser(@Param('id') user_id: string) {
     return this.userService.getUserById(user_id);
   }
-  @Get()
-  getUsers() {
-    return this.userService.getUsers();
+
+  @Post(':id')
+  updateUser(@Param('id') user_id: string, @Body() data: UpdateUserDto) {
+    return this.userService.updateUser(user_id, data);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') user_id: string) {
+    return this.userService.deleteUser(user_id);
   }
 }
