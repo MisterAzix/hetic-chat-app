2945582e3a74ceb615bdbecb9f496a054a8ec17d
balayette_ <louis.fortrie4@gmail.com>
2023-12-10 02:05:55 +0100

    feat(front): wip authenticate and retrieve user data :construction:

--- apps/front/components/forms/LoginForm.tsx
+++ apps/front/components/forms/LoginForm.tsx
@@ -6,11 +6,16 @@ import { BasicButton } from './Button';
 interface FormProps {
   variant: 'login' | 'signup' | 'update';
   onLogin?: () => void;
-  onSignup?: () => void;
+  onSignup?: (email: string, password: string) => void;
   onUpdate?: () => void;
 }
 
-export default function LoginForm({ variant, onLogin, onSignup, onUpdate }: FormProps) {
+export default function LoginForm({
+  variant,
+  onLogin,
+  onSignup,
+  onUpdate,
+}: FormProps) {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const confirmPasswordRef = useRef<HTMLInputElement>(null);
@@ -24,16 +29,16 @@ export default function LoginForm({ variant, onLogin, onSignup, onUpdate }: Form
 
   const toggleFormVariant = () => {
     setFormVariant(formVariant === 'login' ? 'signup' : 'login');
+    console.log(formVariant);
   };
 
   const handleSubmit = () => {
     if (variant === 'login' && onLogin) {
-      onLogin();  
+      onLogin();
     } else if (variant === 'signup' && onSignup) {
-      onSignup(); 
-    }
-    else if (variant === 'update' && onUpdate) {
-      onUpdate(); 
+      onSignup(email, password);
+    } else if (variant === 'update' && onUpdate) {
+      onUpdate();
     }
   };
 
@@ -44,19 +49,22 @@ export default function LoginForm({ variant, onLogin, onSignup, onUpdate }: Form
       flexDirection="column"
       alignItems="start"
       justifyContent="space-around"
-      sx={{width:1, height:1}}
+      sx={{ width: 1, height: 1 }}
     >
-      {variant === 'login' && (<Typography variant="h1">Connexion</Typography>)}
-      {variant === 'signup' && (<Typography variant="h1">Inscription</Typography>)}
-      {variant === 'signup' || variant === 'update' && (
-        <BasicInput
-          label="Nom"
-          forwardedRef={nameRef}
-          variant="outlined"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      {variant === 'login' && <Typography variant="h1">Connexion</Typography>}
+      {variant === 'signup' && (
+        <Typography variant="h1">Inscription</Typography>
       )}
+      {variant === 'signup' ||
+        (variant === 'update' && (
+          <BasicInput
+            label="Nom"
+            forwardedRef={nameRef}
+            variant="outlined"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        ))}
       <BasicInput
         label="Email"
         forwardedRef={emailRef}
@@ -71,24 +79,34 @@ export default function LoginForm({ variant, onLogin, onSignup, onUpdate }: Form
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      {variant === 'signup' || variant === 'update' && (
-        <BasicInput
-          label="Confirmation de mot de passe"
-          forwardedRef={confirmPasswordRef}
-          variant="outlined"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-      )}
+      {variant === 'signup' ||
+        (variant === 'update' && (
+          <BasicInput
+            label="Confirmation de mot de passe"
+            forwardedRef={confirmPasswordRef}
+            variant="outlined"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        ))}
       {variant === 'login' && (
-        <Typography variant="body1">Pas de compte ?<span onClick={toggleFormVariant}>Créez-en un</span></Typography>
-      )
-      }
+        <Typography variant="body1">
+          Pas de compte ?<span onClick={toggleFormVariant}>Créez-en un</span>
+        </Typography>
+      )}
       {variant === 'signup' && (
-        <Typography variant="body1">Vous avez déjà un compte ?<span onClick={toggleFormVariant}>Connectez-vous</span></Typography>
-      )
-      }
-        <BasicButton chat='no' onClick={handleSubmit} buttonText='Valider' buttonVariant='contained' buttonColor='primary' />
+        <Typography variant="body1">
+          Vous avez déjà un compte ?
+          <span onClick={toggleFormVariant}>Connectez-vous</span>
+        </Typography>
+      )}
+      <BasicButton
+        chat="no"
+        onClick={handleSubmit}
+        buttonText="Valider"
+        buttonVariant="contained"
+        buttonColor="primary"
+      />
     </Box>
   );
 }
